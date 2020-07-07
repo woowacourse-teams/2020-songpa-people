b83153b3e25b7f49511abb9184932a33acd4ead3
ordinCode <ysh1346@naver.com>
2020-07-07 16:50:37 +0900

    init

---
+++ src/test/java/com/songpapeople/hashtagmap/HashtagmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songpapeople.hashtagmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashtagmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
