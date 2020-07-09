d1fd6433000640b0ab30d097a297bd5c3e564746
chanin <pci2676@gmail.com>
2020-07-09 11:39:56 +0900

    feat : 기본 패키지 작성 및 HELP.md 제거

---
+++ hashtagmap-web/src/main/java/com/songpapeople/hashtagmap/HashTagMapWebApplication.java
@@ -0,0 +1,11 @@
+package com.songpapeople.hashtagmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashTagMapWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HashTagMapWebApplication.class, args);
+    }
+}
