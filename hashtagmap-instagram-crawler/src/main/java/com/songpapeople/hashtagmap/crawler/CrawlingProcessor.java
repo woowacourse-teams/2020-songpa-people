392f29ed22a1f50818dd38e40d4a8b181d0c8b40
ordinCode <ysh1346@naver.com>
2020-07-14 16:16:55 +0900

    refactor : CrawlingProcessor.java
     - 정제하는 메서드 분리

--- hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/CrawlingProcessor.java
+++ hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/CrawlingProcessor.java
@@ -1,6 +1,47 @@
 package com.songpapeople.hashtagmap.crawler;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // 정제
 public class CrawlingProcessor {
+    private static final String HASHTAG_COUNT_REGEX = "(\"edge_hashtag_to_media\":\\{\"count\"):([0-9]+)";
+    private static final String HASHTAG_POPULAR_INFO_REGEX = "\"edge_hashtag_to_top_posts\":";
+
+    public static String findHashtagCount(String head) {
+        Pattern pattern = Pattern.compile(HASHTAG_COUNT_REGEX);
+        Matcher matcher = pattern.matcher(head);
+
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        throw new IllegalArgumentException("해시태그 카운트를 찾을 수 없습니다.");
+    }
+
+    public static String slicePopularInfo(String body) {
+        int startIdx = body.lastIndexOf(HASHTAG_POPULAR_INFO_REGEX) + HASHTAG_POPULAR_INFO_REGEX.length();
+        String substringBody = body.substring(startIdx);
+
+        int lastIdx = findLastIdx(substringBody);
+
+        return substringBody.substring(0, lastIdx + 1);
+    }
+
+    public static int findLastIdx(String substringBody) {
+        int bracketCount = 0;
+        for (int i = 0; i < substringBody.length(); i++) {
+            if (substringBody.charAt(i) == '{') {
+                bracketCount++;
+                continue;
+            }
+            if (substringBody.charAt(i) == '}') {
+                bracketCount--;
+            }
+            if (bracketCount == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
 
 }
--- hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/InstagramCrawler.java
+++ hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/InstagramCrawler.java
@@ -1,63 +1,21 @@
 package com.songpapeople.hashtagmap.crawler;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.jsoup.nodes.Document;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.songpapeople.hashtagmap.HashtagDto;
+import org.jsoup.nodes.Document;
 
 public class InstagramCrawler {
     private static final String INSTAGRAM_URL_FORMAT = "https://www.instagram.com/explore/tags/%s/?hl=ko";
-    private static final String HASHTAG_COUNT_REGEX = "(\"edge_hashtag_to_media\":\\{\"count\"):([0-9]+)";
-    private static final String HASHTAG_POST_INFO_REGEX = "\"edge_hashtag_to_top_posts\":";
 
     public HashtagDto createHashtagDto(String placeName) {
         Document doc = Crawler.crawling(String.format(INSTAGRAM_URL_FORMAT, placeName));
         String body = doc.body().toString();
-        System.out.println(body);
-        String hashtagCount = findHashtagCount(body);
-        String popularInfo = slicePopularInfo(body);
+        String hashtagCount = CrawlingProcessor.findHashtagCount(body);
+        String popularInfo = CrawlingProcessor.slicePopularInfo(body);
 
         JsonElement json = JsonParser.parseString(popularInfo);
+
         return null;
-    }
-
-    private int findLastIdx(String substringBody) {
-        int bracketCount = 0;
-        for (int i = 0; i < substringBody.length(); i++) {
-            if (substringBody.charAt(i) == '{') {
-                bracketCount++;
-                continue;
-            }
-            if (substringBody.charAt(i) == '}') {
-                bracketCount--;
-            }
-            if (bracketCount == 0) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private String findHashtagCount(String head) {
-        Pattern pattern = Pattern.compile(HASHTAG_COUNT_REGEX);
-        Matcher matcher = pattern.matcher(head);
-
-        if (matcher.find()) {
-            return matcher.group(2);
-        }
-        throw new IllegalArgumentException("해시태그 카운트를 찾을 수 없습니다.");
-    }
-
-    private String slicePopularInfo(String body) {
-        int startIdx = body.lastIndexOf(HASHTAG_POST_INFO_REGEX) + HASHTAG_POST_INFO_REGEX.length();
-        String substringBody = body.substring(startIdx);
-
-        int lastIdx = findLastIdx(substringBody);
-
-        return substringBody.substring(0, lastIdx + 1);
     }
 }
