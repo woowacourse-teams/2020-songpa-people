66085e6f013f4495c4110986a692bf8e5930ccee
JunSeong <ebseud6135@naver.com>
2020-07-15 14:43:22 +0900

    refactor: 메소드 분리 및 Crawler timeout 추가

--- hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/Crawler.java
+++ hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/Crawler.java
@@ -8,11 +8,13 @@ import org.jsoup.nodes.Document;
 public class Crawler {
     private static final String USER_AGENT =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
+    private static final int HOLDING_TIME = 3000;
 
     public static Document crawling(String url) {
         try {
             return Jsoup.connect(url)
                 .userAgent(USER_AGENT)
+                .timeout(HOLDING_TIME)
                 .get();
         } catch (IOException e) {
             throw new IllegalArgumentException("연결할 수 없는 url 입니다.");
--- hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/InstagramCrawler.java
+++ hashtagmap-instagram-crawler/src/main/java/com/songpapeople/hashtagmap/crawler/InstagramCrawler.java
@@ -1,15 +1,16 @@
 package com.songpapeople.hashtagmap.crawler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.nodes.Document;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.songpapeople.hashtagmap.dto.CrawlingDto;
 import com.songpapeople.hashtagmap.dto.PostDto;
-import org.jsoup.nodes.Document;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class InstagramCrawler {
     private static final String INSTAGRAM_URL_FORMAT = "https://www.instagram.com/explore/tags/%s/?hl=ko";
@@ -21,21 +22,22 @@ public class InstagramCrawler {
         String body = document.body().toString();
         String hashtagCount = CrawlingProcessor.searchByRegex(body, HASHTAG_COUNT_REGEX);
         String popularInfo = CrawlingProcessor.searchByRegex(body, HASHTAG_POPULAR_INFO_REGEX);
-
-        JsonElement json = JsonParser.parseString(popularInfo);
-        JsonArray edges = json.getAsJsonObject().get("edges").getAsJsonArray();
-
-        List<PostDto> postDtos = new ArrayList<>();
-        for (int i = 0; i < edges.size(); i++) {
-            JsonObject node = edges.get(i)
-                    .getAsJsonObject().get("node").getAsJsonObject();
-
-            String shortcode = node.get("shortcode").toString();
-            String displayUrl = node.get("display_url").toString();
-
-            postDtos.add(new PostDto(shortcode, displayUrl));
-        }
+        List<PostDto> postDtos = makePostDtos(popularInfo);
 
         return CrawlingDto.of(placeName, hashtagCount, postDtos);
+    }
+
+    private List<PostDto> makePostDtos(String popularInfo) {
+        List<PostDto> postDtos = new ArrayList<>();
+        JsonElement json = JsonParser.parseString(popularInfo);
+        JsonArray edges = json.getAsJsonObject().get("edges").getAsJsonArray();
+        for (JsonElement edge : edges) {
+            JsonObject node = edge.getAsJsonObject().get("node").getAsJsonObject();
+
+            String shortCode = node.get("shortcode").toString();
+            String displayUrl = node.get("display_url").toString();
+            postDtos.add(new PostDto(shortCode, displayUrl));
+        }
+        return postDtos;
     }
 }
