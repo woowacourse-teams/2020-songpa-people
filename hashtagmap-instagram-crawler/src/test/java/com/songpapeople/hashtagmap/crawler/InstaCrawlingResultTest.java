5406a487486e82a8c97c04e20ba45a5cefb4bb26
ordinCode <ysh1346@naver.com>
2020-07-21 15:52:43 +0900

    feat: InstaCrawlingResult 테스트 추가

--- hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/InstaCrawlingResultTest.java
+++ hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/InstaCrawlingResultTest.java
@@ -1,13 +1,40 @@
 package com.songpapeople.hashtagmap.crawler;
 
+import com.songpapeople.hashtagmap.dto.PostDtos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class InstaCrawlingResultTest {
-    @DisplayName("body에서 hashtag개수 가져오는 기능 테스트")
-    @Test
-    void findHashTagCount() {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.*;
+
+class InstaCrawlingResultTest {
+    @DisplayName("body에서 인기게시물 정보를 가져와 PostDtos를 만드는 기능 테스트")
+    @Test
+    void findPostDtos() {
+        String body = createbody();
+        InstaCrawlingResult instaCrawlingResult = new InstaCrawlingResult(body);
+
+        PostDtos postDtos = instaCrawlingResult.findPostDtos();
+
+        assertThat(postDtos.size()).isEqualTo(PostDtos.POPULAR_POST_SIZE);
     }
 
+    String createbody() {
+        StringBuilder builder = new StringBuilder();
+        try (FileReader fileReader = new FileReader(new File("src/test/resources/crawling-mock-data.txt"))) {
+            while (true) {
+                int fileData = fileReader.read();
+                if (fileData == -1) {
+                    break;
+                }
+                builder.append((char) fileData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
 }
