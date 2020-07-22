89fff79614e51a48645b73314c9db5e893d0f6b2
JunSeong <ebseud6135@naver.com>
2020-07-22 20:13:37 +0900

    feat: App.vue에 KakaoMap 추가

--- hashtagmap-web/front/vue.config.js
+++ hashtagmap-web/front/vue.config.js
@@ -1,5 +1,6 @@
+const path = require("path");
+
 module.exports = {
-  outputDir:
-    "C:\\Users\\ebseu\\Documents\\Git\\2020-songpa-people\\hashtagmap-web\\src\\main\\resources\\static",
+  outputDir: path.resolve("__dirname", "../../src/main/resources/static"),
   transpileDependencies: ["vuetify"],
 };
