43b9c44506d6b8b2a750c746ff874de5fbf3ce9b
JunSeong <ebseud6135@naver.com>
2020-07-23 13:12:49 +0900

    style: prettier 적용

--- hashtagmap-web/front/src/App.vue
+++ hashtagmap-web/front/src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <v-app>
     <v-main>
-        <KakaoMap/>
+      <KakaoMap />
     </v-main>
   </v-app>
 </template>
 
 <script>
-  import KakaoMap from "@/views/KakaoMap";
+import KakaoMap from "@/views/KakaoMap";
 
-  export default {
-    name: "App",
+export default {
+  name: "App",
 
   components: {
     KakaoMap,
   },
 
-    data: () => ({
-      //
-    }),
+  data: () => ({
+    //
+  }),
 };
 </script>
 
 <style lang="stylus">
-    #app
-        font-family Avenir, Helvetica, Arial, sans-serif
-        -webkit-font-smoothing antialiased
-        -moz-osx-font-smoothing grayscale
-        text-align center
-        color #2c3e50
-        margin-top 60px
+#app
+    font-family Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
 </style>
