e82adee118b4855c4a6889be94c937ae5153efcd
giantim <giantim132@gmail.com>
2020-07-27 14:23:23 +0900

    refactor: lombok @EqualsAndHashCode 어노테이션 제거

--- hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/place/domain/model/Location.java
+++ hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/place/domain/model/Location.java
@@ -1,14 +1,14 @@
 package com.songpapeople.hashtagmap.place.domain.model;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
+import java.util.Objects;
 
-@EqualsAndHashCode
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,4 +18,24 @@ public class Location {
     private Point point;
 
     private String roadAddressName;
+
+    @Generated
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Location location = (Location) o;
+        return Objects.equals(point, location.point)
+                && Objects.equals(roadAddressName, location.roadAddressName);
+    }
+
+    @Generated
+    @Override
+    public int hashCode() {
+        return Objects.hash(point, roadAddressName);
+    }
 }
--- hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/place/domain/model/Point.java
+++ hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/place/domain/model/Point.java
@@ -3,8 +3,8 @@ package com.songpapeople.hashtagmap.place.domain.model;
 import lombok.*;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
-@EqualsAndHashCode
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -12,4 +12,24 @@ import javax.persistence.Embeddable;
 public class Point {
     private String latitude;
     private String longitude;
+
+    @Generated
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Objects.equals(latitude, point.latitude)
+                && Objects.equals(longitude, point.longitude);
+    }
+
+    @Generated
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
 }
