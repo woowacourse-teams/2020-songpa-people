8ec6df6205983820115856827e63e6f9dd1b3146
ordinCode <ysh1346@naver.com>
2020-07-28 17:03:34 +0900

    feat: InstagramScheduleService 어노테이션 @Service로 변경

--- hashtagmap-instagram-scheduler/src/main/java/com/songpapeople/hashtagmap/scheduler/InstagramScheduleService.java
+++ hashtagmap-instagram-scheduler/src/main/java/com/songpapeople/hashtagmap/scheduler/InstagramScheduleService.java
@@ -3,13 +3,13 @@ package com.songpapeople.hashtagmap.scheduler;
 import com.songpapeople.hashtagmap.crawler.InstagramCrawler;
 import com.songpapeople.hashtagmap.place.domain.model.Place;
 import com.songpapeople.hashtagmap.proxy.ProxiesFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class InstagramScheduleService {
     private static final int START_TRY_COUNT = 0;
 
