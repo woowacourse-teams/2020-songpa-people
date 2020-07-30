c79a07c4ddf5890e4052e0de2423fdd76d5798e9
ordinCode <ysh1346@naver.com>
2020-07-30 14:55:21 +0900

    feat: 홈화면 명소 이미지 추가

--- hashtagmap-admin/front/src/App.vue
+++ hashtagmap-admin/front/src/App.vue
@@ -2,6 +2,14 @@
   <v-app id="app">
     <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
+        <v-list-item to="/">
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item to="/kakao-scheduler">
           <v-list-item-action>
             <v-icon>mdi-view-dashboard</v-icon>
@@ -21,10 +29,10 @@
       </v-list>
     </v-navigation-drawer>
 
-<!--    <v-app-bar app clipped-left height="40px">-->
-<!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>-->
-<!--      <v-toolbar-title>명소를 찾아서</v-toolbar-title>-->
-<!--    </v-app-bar>-->
+    <v-app-bar app clipped-left height="40px">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>명소를 찾아서</v-toolbar-title>
+    </v-app-bar>
 
     <v-main>
       <router-view />
--- hashtagmap-admin/front/src/views/Home.vue
+++ hashtagmap-admin/front/src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
   <v-container>
-    <v-card>
-      <v-img src="src/assets/bright-cow-image.png"> </v-img>
-    </v-card>
+      <v-img id="image" :src="faviconPath" contain width="400px" />
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "InstagramScheduler"
+  name: "InstagramScheduler",
+  data() {
+    return {
+      faviconPath: require("../assets/bright-cow-image.png")
+    };
+  }
 };
 </script>
+
+<style scoped>
+#image {
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 30%);
+}
+</style>
