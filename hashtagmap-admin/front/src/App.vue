4bdc52bfd17eae6adbb4302fa2e3e077e3fce18c
ordinCode <ysh1346@naver.com>
2020-07-30 13:24:13 +0900

    feat: vue 프로젝트 초기화

---
+++ hashtagmap-admin/front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view id="router-view" />
+  </div>
+</template>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+  #nav {
+    border: aquamarine solid 3px;
+  }
+
+  #router-view {
+    border: red solid 3px;
+  }
+</style>
