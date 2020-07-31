6dd56f9400e2b34d4aa40187c5bc90d29a171459
ordinCode <ysh1346@naver.com>
2020-07-31 14:57:08 +0900

    refactor: 주석제거

--- hashtagmap-admin/front/src/router/index.js
+++ hashtagmap-admin/front/src/router/index.js
@@ -20,9 +20,6 @@ const routes = [
   {
     path: "/instagram-scheduler",
     name: "InstagramScheduler",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: InstagramScheduler
   }
 ];
