65450a04a1433989dc9c698480887728a8d75af5
moonyoungCHAE <xpf_fl@naver.com>
2020-08-03 19:02:04 +0900

    fixes: 잘못된 가게 이름 데이터 가공 수정

--- hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/InstagramCrawlerTest.java
+++ hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/InstagramCrawlerTest.java
@@ -29,7 +29,7 @@ class InstagramCrawlerTest {
 
     @DisplayName("가게 이름 데이터 가공하기")
     @ParameterizedTest
-    @CsvSource({"스타벅스 강남역점,스타벅스강남", "피자나라 치킨공주,피자나라치킨공주"})
+    @CsvSource({"스타벅스 강남역점,스타벅스강남역", "피자나라 치킨공주,피자나라치킨공주"})
     void parsePlaceName(String placeName, String expected) {
         InstagramCrawler instagramCrawler = new InstagramCrawler();
         String actual = instagramCrawler.parsePlaceName(placeName);
