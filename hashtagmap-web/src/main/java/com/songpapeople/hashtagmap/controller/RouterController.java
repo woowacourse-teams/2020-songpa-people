4e0b45e3078ee66a20d6c1516f070a92918e232a
JunSeong <ebseud6135@naver.com>
2020-08-04 14:08:14 +0900

    feat: RouteController 추가
     - 에러 발생 시 index.html로 리턴
     - application.yml에 .html을 subfix로 추가

---
+++ hashtagmap-web/src/main/java/com/songpapeople/hashtagmap/controller/RouterController.java
@@ -0,0 +1,18 @@
+package com.songpapeople.hashtagmap.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RouterController implements ErrorController {
+    @GetMapping({"/", "/error"})
+    public String getIndex() {
+        return "index";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
