12ca6f53a4cf0012d742b4d81cb28e7d2656fdbd
ordinCode <ysh1346@naver.com>
2020-08-05 15:46:32 +0900

    refactor: RouterController가 errorController를 implements하도록 수정

--- hashtagmap-admin/src/main/java/com/songpapeople/hashtagmap/controller/RouterController.java
+++ hashtagmap-admin/src/main/java/com/songpapeople/hashtagmap/controller/RouterController.java
@@ -1,12 +1,18 @@
 package com.songpapeople.hashtagmap.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RouterController {
-    @GetMapping(value = {"/", "/instagram-scheduler"})
+public class RouterController implements ErrorController {
+    @GetMapping({"/", "/error"})
     public String index() {
-        return "index.html";
+        return "index";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
