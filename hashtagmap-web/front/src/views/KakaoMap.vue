d46dd2d57b73234ca5d06bded6a30e3f9a5adcc3
giantim <giantim132@gmail.com>
2020-08-14 02:50:04 +0900

    feat: 태그 레벨 필터에 맞는 가게 마커만 출력

--- hashtagmap-web/front/src/views/KakaoMap.vue
+++ hashtagmap-web/front/src/views/KakaoMap.vue
@@ -14,23 +14,32 @@ import { getMarkerImage, SIZE } from "../utils/markerImages";
 
 export default {
   name: "KakaoMap",
-  async mounted() {
+  async created() {
     this.SET_KAKAO_MAP_API(await this.$initKakaoMapApi());
     this.SET_KAKAO_MAP(this.$loadMap());
     await this.setPlaces();
     this.loadMarker();
     this.$loadCurrentPosition();
   },
+
+  mounted() {
+    this.$store.watch(() => {
+      this.$store.getters.activeMarker();
+    });
+  },
+
   computed: {
     ...mapState(["kakaoMap", "kakaoMapApi", "places"]),
   },
+
   methods: {
-    ...mapMutations(["SET_KAKAO_MAP_API", "SET_KAKAO_MAP"]),
+    ...mapMutations(["SET_KAKAO_MAP_API", "SET_KAKAO_MAP", "ADD_MARKER"]),
     ...mapActions(["setDetailModal", "setPlaces"]),
     loadMarker() {
       this.places.map(place => {
         const marker = this.createMaker(place);
         marker.setMap(this.kakaoMap);
+        this.ADD_MARKER(marker);
         const textBalloon = this.createTextBalloon(place, marker);
         this.kakaoMapApi.event.addListener(marker, EVENT_TYPE.CLICK, () => {
           this.onAddTextBalloonToMarker(this.kakaoMap, place, textBalloon);
@@ -71,6 +80,7 @@ export default {
       });
     },
   },
+
   components: {
     DetailModal,
   },
