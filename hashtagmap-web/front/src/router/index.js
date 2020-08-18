ca943d7dceebede39b3f79ef441896c4278684bd
moonyoungCHAE <xpf_fl@naver.com>
2020-08-18 16:52:20 +0900

    feat: main page 뼈대 작성

--- hashtagmap-web/front/src/router/index.js
+++ hashtagmap-web/front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import KakaoMap from "@/views/KakaoMap.vue";
+import MainPage from "@/views/MainPage.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/map",
     name: "KakaoMap",
     component: KakaoMap,
+  },
+  {
+    path: "/",
+    name: "MainPage",
+    component: MainPage,
   },
 ];
 
---
+++ hashtagmap-web/front/src/views/MainPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-main>
+      <swiper class="swiper" :options="swiperOption">
+        <swiper-slide class="content"> <MainPageButton /> </swiper-slide>
+        <swiper-slide class="content">Slide 2</swiper-slide>
+        <swiper-slide class="content">Slide 3</swiper-slide>
+        <swiper-slide class="content">Slide 4</swiper-slide>
+        <swiper-slide class="content">Slide 5</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainPageButton from "@/components/main-page/MainPageButton";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/swiper-bundle.css";
+
+export default {
+  name: "swiper-example-vertical",
+  title: "Vertical slider",
+  components: {
+    Swiper,
+    SwiperSlide,
+    MainPageButton,
+  },
+  data() {
+    return {
+      swiperOption: {
+        direction: "vertical",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+.content {
+  width: 100%;
+  height: 100%;
+}
+</style>
