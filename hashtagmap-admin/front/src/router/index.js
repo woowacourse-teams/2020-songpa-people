8d17a30cc8425c0355e5b3e81f5ed508f54ce74a
moonyoungCHAE <xpf_fl@naver.com>
2020-08-18 13:58:59 +0900

    fix: build 오류 fix

--- hashtagmap-admin/front/src/router/index.js
+++ hashtagmap-admin/front/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home";
 import KakaoScheduler from "../views/KakaoScheduler";
 import InstagramScheduler from "../views/InstagramScheduler";
 import TagLevel from "../views/TagLevel";
--- hashtagmap-web/src/main/java/com/songpapeople/hashtagmap/exception/WebException.java
+++
@@ -1,7 +0,0 @@
-package com.songpapeople.hashtagmap.exception;
-
-public class WebException extends HashtagMapException {
-    public WebException(WebExceptionStatus webExceptionStatus) {
-        super(webExceptionStatus.getMessage(), webExceptionStatus.getCode());
-    }
-}
