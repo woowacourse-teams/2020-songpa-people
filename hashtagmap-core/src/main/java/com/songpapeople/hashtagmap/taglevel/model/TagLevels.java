ad020b5bfa710e401095549ad4842d7a1205ae94
moonyoungCHAE <xpf_fl@naver.com>
2020-08-21 11:03:28 +0900

    refactor: 불필요한 annotation 제거

--- hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/taglevel/model/TagLevels.java
+++ hashtagmap-core/src/main/java/com/songpapeople/hashtagmap/taglevel/model/TagLevels.java
@@ -1,10 +1,7 @@
 package com.songpapeople.hashtagmap.taglevel.model;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
 
-@Slf4j
 public class TagLevels {
     private final List<TagLevel> tagLevels;
 
