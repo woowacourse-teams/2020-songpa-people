c51ddc0b54f545f3752f3c74c0456e910aeaf8d8
ordinCode <ysh1346@naver.com>
2020-08-25 08:41:51 +0900

    refactor: 충돌해결

--- hashtagmap-instagram-scheduler/src/test/java/com/songpapeople/hashtagmap/scheduler/CrawlerWithProxyTest.java
+++ hashtagmap-instagram-scheduler/src/test/java/com/songpapeople/hashtagmap/scheduler/CrawlerWithProxyTest.java
@@ -3,12 +3,14 @@ package com.songpapeople.hashtagmap.scheduler;
 import com.songpapeople.hashtagmap.crawler.InstagramCrawler;
 import com.songpapeople.hashtagmap.exception.InstagramSchedulerException;
 import com.songpapeople.hashtagmap.place.domain.model.Place;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
@@ -27,9 +29,12 @@ class CrawlerWithProxyTest {
     @Mock
     private ProxySetter proxySetter;
 
+    @Mock
+    private InstagramScheduleService instagramScheduleService;
+
     @BeforeEach
     void setUp() {
-        crawlerWithProxy = new CrawlerWithProxy(proxySetter, instagramCrawler);
+        crawlerWithProxy = new CrawlerWithProxy(proxySetter, instagramCrawler, instagramScheduleService);
     }
 
     @DisplayName("검색할 수 없는 Place 검색시 Optional.empty()반환")
@@ -39,6 +44,7 @@ class CrawlerWithProxyTest {
                 .placeName("잠실타로&사주")
                 .build();
 
+        when(instagramScheduleService.findHashtagNameToCrawl(place)).thenReturn(place.getPlaceName());
         when(instagramCrawler.crawler(place.getPlaceName()))
                 .thenThrow(InstagramSchedulerException.class);
 
