b964783a5e2627b5a5c41683e0d65b76beed8e28
ordinCode <ysh1346@naver.com>
2020-09-10 18:17:52 +0900

    feat: health check 수

--- script/nonstop-deploy.sh
+++ script/nonstop-deploy.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-BASE_PATH=~/
-BUILD_PATH=$(ls $BASE_PATH/app/nonstop/*.jar)
+BUILD_PATH=$(ls ~/app/nonstop/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
@@ -13,18 +12,18 @@ echo "> $CURRENT_PROFILE"
 
 sleep 1
 # 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
-if [ $CURRENT_PROFILE == "set1" ]
+if [ $CURRENT_PROFILE == set1 ]
 then
-  IDLE_PROFILE=set2
+  IDLE_PROFILE="set2"
   IDLE_PORT=8082
-elif [ $CURRENT_PROFILE == "set2" ]
+elif [ $CURRENT_PROFILE == set2 ]
 then
-  IDLE_PROFILE=set1
+  IDLE_PROFILE="set1"
   IDLE_PORT=8081
 else
   echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
   echo "> set1을 할당합니다. IDLE_PROFILE: set1"
-  IDLE_PROFILE=set1
+  IDLE_PROFILE="set1"
   IDLE_PORT=8081
 fi
 echo "> IDLE_PORT 확인 : $IDLE_PORT"
@@ -39,20 +38,38 @@ then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
-  kill -15 $IDLE_PID
-  sleep 5
+  sudo kill -15 $IDLE_PID
+  echo "> 20초 뒤에 배포 시작"
+  sleep 20
 fi
 
+cd ~/app/nonstop
 echo "> $IDLE_PROFILE 배포"
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $BUILD_PATH &
+nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME &
+
+#sleep 10;
+
+# switch.sh
+#echo "> 스위칭"
+#echo "> 현재 구동중인 Port 확인"
+#echo "> 현재 배포중인 profile : $CURRENT_PROFILE"
+#
+#echo "> 전환할 Port: $IDLE_PORT"
+#echo "> Port 전환"
+#echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+#
+#sleep 3
+#
+#echo "> Nginx restart"
+#sudo service nginx restart
 
 echo "> $IDLE_PROFILE 10초 후 Health check 시작"
-echo "> curl -s http://localhost:$IDLE_PORT/health "
+echo "> curl -s http://localhost:$IDLE_PORT/actuator/health "
 sleep 10
 
 for retry_count in {1..10}
 do
-  response=$(curl -s http://localhost:$IDLE_PORT/health)
+  response=$(curl -s http://localhost:$IDLE_PORT/actuator/health)
   up_count=$(echo $response | grep '미소' | wc -l)
 
   if [ $up_count -ge 1 ]
@@ -75,7 +92,7 @@ do
   sleep 10
 done
 
-# switch.sh
+ switch.sh
 echo "> 스위칭"
 echo "> 현재 구동중인 Port 확인"
 echo "> 현재 배포중인 profile : $CURRENT_PROFILE"
@@ -84,8 +101,12 @@ echo "> 전환할 Port: $IDLE_PORT"
 echo "> Port 전환"
 echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "> Nginx Reload"
-sudo service nginx reload
+sleep 1
+
+echo "> Nginx restart"
+sudo service nginx restart
+
+sleep 2
 
 PROXY_PORT=$(curl -s http://localhost/profile)
 echo "> Nginx Current Proxy Port: $PROXY_PORT"
