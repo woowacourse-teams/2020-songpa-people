99251ee6d0e73a30a36de96d939131ad1892b9ef
JunSeong <ebseud6135@naver.com>
2020-09-16 17:21:32 +0900

    feat: js, img, css 리소스만 캐싱하도록 수정, Etag 테스트 작성

--- hashtagmap-web/src/test/java/com/songpapeople/hashtagmap/web/StaticResourceTest.java
+++ hashtagmap-web/src/test/java/com/songpapeople/hashtagmap/web/StaticResourceTest.java
@@ -1,6 +1,7 @@
 package com.songpapeople.hashtagmap.web;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StaticResourceTest {
@@ -28,10 +31,11 @@ public class StaticResourceTest {
     @Test
     void cacheResource() {
         given()
-                .accept(MediaType.TEXT_HTML_VALUE)
                 .when()
-                .get("/index.html")
+                .accept("image/png")
+                .get("/img/icons/menu-bar-icon.png")
                 .then()
+                .contentType("image/png")
                 .log().all()
                 .statusCode(HttpStatus.OK.value())
                 .assertThat().header("Cache-Control", "max-age=31536000, public");
@@ -46,7 +50,39 @@ public class StaticResourceTest {
                 .get("/index.html")
                 .then()
                 .log().all()
+                .contentType("text/html")
                 .statusCode(HttpStatus.OK.value())
                 .assertThat().header("Content-Encoding", "gzip");
+    }
+
+    @DisplayName("리소스 응답 Etag 반환 테스트")
+    @Test
+    void etagResource() {
+        Response response = given()
+                .accept("image/png")
+                .when()
+                .get("/img/icons/menu-bar-icon.png");
+
+        String etagValue = response.getHeader("Etag");
+
+        assertThat(etagValue).isNotNull();
+    }
+
+    @DisplayName("리소스 두 번째 요청 304응답 테스트")
+    @Test
+    void etagRedirectResource() {
+        Response response = given()
+                .accept("image/png")
+                .when()
+                .get("/img/icons/menu-bar-icon.png");
+        String etagValue = response.getHeader("Etag");
+
+        given()
+                .header("If-None-Match", etagValue)
+                .when()
+                .get("/img/icons/menu-bar-icon.png")
+                .then()
+                .log().all()
+                .statusCode(HttpStatus.NOT_MODIFIED.value());
     }
 }
