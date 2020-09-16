187f911109752dba7d2591e9e1b5d033d1b7c6cc
JunSeong <ebseud6135@naver.com>
2020-09-16 13:07:14 +0900

    feat: 모든 리소스 파일에 캐싱 주기 1년 적용 및 ETag 적용

---
+++ hashtagmap-web/src/main/java/com/songpapeople/hashtagmap/config/WebMvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.songpapeople.hashtagmap.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(60 * 60 * 24 * 365, TimeUnit.SECONDS).cachePublic());
+    }
+
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter etagHeaderFilter = new ShallowEtagHeaderFilter();
+        registration.setFilter(etagHeaderFilter);
+        registration.addUrlPatterns("/**");
+        return registration;
+    }
+}
