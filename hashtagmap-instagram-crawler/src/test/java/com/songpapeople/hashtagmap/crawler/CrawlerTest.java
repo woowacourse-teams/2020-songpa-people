f068dccaf4f2ef30e93478808a5c44be771f769b
moonyoungCHAE <xpf_fl@naver.com>
2020-10-02 10:32:58 +0900

    test: 크롤링 통신 mock 처리

--- hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/CrawlerTest.java
+++ hashtagmap-instagram-crawler/src/test/java/com/songpapeople/hashtagmap/crawler/CrawlerTest.java
@@ -2,71 +2,90 @@ package com.songpapeople.hashtagmap.crawler;
 
 import com.songpapeople.hashtagmap.exception.CrawlerException;
 import com.songpapeople.hashtagmap.exception.CrawlerExceptionStatus;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CrawlerTest {
-    private Crawler crawler;
+    private final Crawler crawler = new Crawler();
+    private ClientAndServer mockServer;
 
     @BeforeEach
-    private void setUp() {
-        crawler = new Crawler();
+    void setUp() {
+        mockServer = ClientAndServer.startClientAndServer();
     }
 
-    @Disabled
     @DisplayName("크롤링 정상 동작 테스트")
-    @Order(1)
     @Test
     void crawlingTest() {
-        String body = crawler.crawl("https://www.naver.com/");
+        new MockServerClient("localhost", mockServer.getPort())
+                .when(request()
+                        .withMethod("GET"))
+                .respond(response()
+                        .withStatusCode(200));
+
+        String body = crawler.crawl("http://localhost:" + mockServer.getPort());
         assertThat(body).isNotNull();
     }
 
     @DisplayName("잘못된 url 크롤링 시 예외 테스트")
-    @Order(2)
     @Test
     void crawlingUrlExceptionTest() {
-        assertThatThrownBy(() -> crawler.crawl("https://www.asd!@#ASDQQWE1.com/"))
+        new MockServerClient("localhost", mockServer.getPort())
+                .when(request()
+                        .withMethod("GET"))
+                .respond(response()
+                        .withStatusCode(400));
+
+        assertThatThrownBy(() -> crawler.crawl("http://localhost:" + mockServer.getPort() + "/"))
                 .isInstanceOf(CrawlerException.class)
                 .hasMessage(CrawlerExceptionStatus.URL_NOT_CONNECT.getMessage());
     }
 
     @DisplayName("찾을 수 없는 URL 예외 테스트")
     @Test
-    @Order(3)
     void notFoundUrlExceptionTest() {
-        assertThatThrownBy(() -> crawler.crawl("https://www.instagram.com/explore/tags/절대로_있을_리가_없는_검색어_1234/"))
+        final String KEY_WORD = "절대로_없을_검색어";
+
+        new MockServerClient("localhost", mockServer.getPort())
+                .when(request()
+                        .withMethod("GET")
+                        .withPath("/explore/tags/" + KEY_WORD + "/"))
+                .respond(response()
+                        .withStatusCode(429));
+
+        assertThatThrownBy(() -> crawler.crawl("http://localhost:" + mockServer.getPort() + "/explore/tags/" + KEY_WORD + "/"))
                 .isInstanceOf(CrawlerException.class)
                 .hasMessage(CrawlerExceptionStatus.NOT_FOUND_URL.getMessage());
     }
 
     @DisplayName("너무 많은 요청을 보내 발생하는 429 예외 테스트")
     @Test
-    @Order(4)
     void tooManyRequestExceptionTest() {
-        List<Integer> numbers = IntStream.range(0, 200).boxed().collect(Collectors.toList());
-        try {
-            numbers.parallelStream()
-                    .map(num -> crawler.crawl("https://www.instagram.com/explore/tags/" + num + "/")).count();
-        } catch (CrawlerException e) {
+        final String KEY_WORD = "daily";
 
-        }
+        new MockServerClient("localhost", mockServer.getPort())
+                .when(request()
+                        .withMethod("GET")
+                        .withPath("/explore/tags/" + KEY_WORD + "/"))
+                .respond(response()
+                        .withStatusCode(429));
 
-        assertThatThrownBy(() -> crawler.crawl("https://www.instagram.com/explore/tags/daily/"))
+        assertThatThrownBy(() -> crawler.crawl("http://localhost:" + mockServer.getPort() + "/explore/tags/" + KEY_WORD + "/"))
                 .isInstanceOf(CrawlerException.class)
                 .hasMessage(CrawlerExceptionStatus.TOO_MANY_REQUEST.getMessage());
+    }
+
+    @AfterEach
+    void shutDown() {
+        mockServer.stop();
     }
 }
